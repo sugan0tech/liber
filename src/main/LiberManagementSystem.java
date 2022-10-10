5bd8024be7e2919b25d4e39d1ad9845de3a17978
sugan0tech <sugankpms@gmail.com>
2022-10-10 22:12:53 +0530

    added menu bars and sample panel output closes #1

--- src/main/LiberManagementSystem.java
+++ src/main/LiberManagementSystem.java
@@ -3,21 +3,34 @@ package main;
 import java.awt.Frame;
 import java.awt.Menu;
 import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+import panel.LoginPanel;
 import panel.Sample;
 
 public class LiberManagementSystem {
-	private static boolean sessionFlag;
+
+	private static boolean sessionFlag = false;
+	
+
 	public static void main(String[] args) {
 		Frame frame = new Frame();
 		// sample panel will be removed
-		frame.add(new Sample().getPanel());
 		
 		MenuBar bar = new MenuBar();
 		Menu login = new Menu("login");
 		bar.add(login);
+		Menu bookManager = new Menu("book Manager");
+		bar.add(bookManager);
+		Menu bookIssue = new Menu("book issue");
+		bar.add(bookIssue);
+		Menu bookReturn = new Menu("book Return");
+		bar.add(bookReturn);
+
+		frame.add(new Sample().getPanel());
 		
 		// set border size as 20 for each panel
 		frame.setSize(1000, 1000);
