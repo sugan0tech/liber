f9c4487b68e6f8082678cea46f9da9ff890577ab
sugan0tech <sugankpms@gmail.com>
2022-10-12 19:23:12 +0530

    converted static to final properties

--- src/repository/LiberMainJDBC.java
+++ src/repository/LiberMainJDBC.java
@@ -3,9 +3,9 @@ package repository;
 import java.sql.*;
 
 public class LiberMainJDBC {
-	   static final String DB_URL = "jdbc:mysql://localhost/LiberMain";
-	   static final String USER = "sugan";
-	   static final String PASS = "1234";
+	   private final String DB_URL = "jdbc:mysql://localhost/LiberMain";
+	   private final String USER = "sugan";
+	   private final String PASS = "1234";
 
 	   public Statement getStatement() throws Exception {
 	      Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
